acc7353ca5bab87f362a122d8d787a019e0cec7d
Igor Jovanovic <igorjovanovic2898@gmail.com>
2021-11-22 13:55:46 +0100

    move recv to another thread, threads are now stack allocated

--- stp_concurrent/src/stp_concurrent.cpp
+++ stp_concurrent/src/stp_concurrent.cpp
@@ -1,32 +1,47 @@
 #include "stp_concurrent.hpp"
 
-Switch::Switch(uint32_t bridgeId) : root_id(bridgeId), bridge_id(bridgeId) {
-  recvQ = std::make_shared<queue>();
-}
-void link(Switch& a, Switch& b) {
+Switch::Switch(uint32_t bridgeId) : root_id(bridgeId), bridge_id(bridgeId) {}
+void link(Switch &a, Switch &b) {
   a.sendQ.push_back(b.recvQ);
   b.sendQ.push_back(a.recvQ);
 }
-void Switch::start() {
-  while (true) {
+void Switch::startSend() {
+  while (!terminate.load()) {
     for (auto a : sendQ) {
-      a->enqueue(root_id);
+      a.get().enqueue(root_id);
     }
-    std::array<uint32_t,20> neighbors;
-    std::size_t count;
-    while ((count = recvQ->try_dequeue_bulk(neighbors.data(), 20)) != 0) {
-      for (std::size_t i = 0; i != count; ++i) {
-        if (neighbors[i] < root_id) root_id = neighbors[i];
-        std::stringstream ss;
-        ss << "Switch#" << bridge_id << " received rootID " << neighbors[i]
-           << " :: value of rootID = " << root_id << std::endl;
-        std::cout << ss.str();
-      }
-    }
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(hello_timer));
   }
 }
-
+void Switch::startRecv() {
+  while (!terminate.load()) {
+    std::array<uint32_t, 20> neighbors;
+    std::size_t count;
+    while ((count = recvQ.try_dequeue_bulk(neighbors.data(), 20)) != 0) {
+      for (std::size_t i = 0; i != count; ++i) {
+        if (neighbors[i] < root_id) root_id = neighbors[i];
+        spdlog::info(
+            "Switch#{0:d} received rootID {1:d} :: value of rootID={2:d}",
+            this->bridge_id, neighbors[i], this->root_id);
+      }
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(hello_timer));
+  }
+}
+void Switch::startSwitch() {
+  terminate.store(false);
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution<uint> dist(1, 15);
+  hello_timer = dist(mt);
+  sendTh = std::move(std::thread(&Switch::startSend, this));
+  recvTh = std::move(std::thread(&Switch::startRecv, this));
+}
+void Switch::stopSwitch() {
+  terminate.store(true);
+  sendTh.join();
+  recvTh.join();
+}
 int main() {
   Switch a(1000);
   Switch b(300);
@@ -38,17 +53,17 @@ int main() {
   link(b, d);
   link(b, e);
   link(e, d);
-  std::thread s1(&Switch::start, &a);
-  std::thread s2(&Switch::start, &b);
-  std::thread s3(&Switch::start, &c);
-  std::thread s4(&Switch::start, &d);
-  std::thread s5(&Switch::start, &e);
 
-  s1.join();
-  s2.join();
-  s3.join();
-  s4.join();
-  s5.join();
-
+  a.startSwitch();
+  b.startSwitch();
+  c.startSwitch();
+  d.startSwitch();
+  e.startSwitch();
+  std::this_thread::sleep_for(std::chrono::seconds(60));
+  a.stopSwitch();
+  b.stopSwitch();
+  c.stopSwitch();
+  d.stopSwitch();
+  e.stopSwitch();
   return 0;
 }
--- stp_concurrent/src/stp_concurrent.hpp
+++ stp_concurrent/src/stp_concurrent.hpp
@@ -1,18 +1,29 @@
+#include <atomic>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <vector>
 
 #include "concurrentqueue.hpp"
-using queue = moodycamel::ConcurrentQueue<int>;
+#include "spdlog/spdlog.h"
+using queue = moodycamel::ConcurrentQueue<uint32_t>;
 
 class Switch {
  public:
   Switch(uint32_t bridgeId);
-  friend void link(Switch& a,Switch& b);
-  void start();
+  friend void link(Switch &a, Switch &b);
+  void startSwitch();
+  void stopSwitch();
+
  private:
+  void startSend();
+  void startRecv();
   uint32_t root_id;
   uint32_t bridge_id;
-  std::shared_ptr<queue> recvQ;
-  std::vector<std::shared_ptr<queue>> sendQ;
+  uint hello_timer;
+  std::atomic_bool terminate{false};
+  std::thread recvTh;
+  std::thread sendTh;
+  queue recvQ;
+  std::vector<std::reference_wrapper<queue>> sendQ;
 };
